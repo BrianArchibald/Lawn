f87cf1d279ef1f030ee8c2c43136b2374981be6a
BrianArchibald <brianallenarchibald@gmail.com>
2018-07-24 19:29:06 -0500

    added contact form, responsive work
    set up mobile nav menu

--- lawn.js
+++ lawn.js
@@ -46,8 +46,24 @@ function debounce(func, wait = 20, immediate = true) {
 // }
 
 // window.addEventListener('scroll', fixedBottomNav);
+const nav = document.querySelector('.navbar');
+const mobileNavIcon = document.querySelector('#menu-icon');
+const mobileNavList = document.querySelector('#mobile-nav');
+const closeMenu = document.querySelector('#close-menu');
 
-
+function changeMobileNav() {
+  if (nav.style.display != "none") {
+    mobileNavIcon.style.display = "none";
+    nav.style.display = "none";
+    mobileNavList.style.display = "block";
+  } else {
+    mobileNavIcon.style.display = "inline-block";
+    nav.style.display = "grid";
+    mobileNavList.style.display = "none";
+  }
+}
+mobileNavIcon.addEventListener('click', changeMobileNav);
+closeMenu.addEventListener('click', changeMobileNav);
 
 
 
